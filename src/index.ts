105c9641f243a36729fc05921fe693876fb63032
Abhinav Viswam <141334489+AbhinavViswam@users.noreply.github.com>
2025-08-21 12:58:04 +0530

    fix 123

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ const server = Fastify();
 
 const port: number = parseInt(process.env.PORT || "4000");
 
-server.register(fastifyCookie);
 server.register(cors, {
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 });
 
+server.register(fastifyCookie);
 registerRoutes(server);
 connectDb()
   .then(() => console.log("DB connected"))
